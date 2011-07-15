52415a3a79c573e309fd93f99578db1d76c23a2f
gamosoft_cp <gamosoft@users.noreply.github.com>
2011-07-15 22:13:30 +0000

    Initial upload of the project

---
+++ EntLibExtensionsTest/Main.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace EntLibExtensionsTest
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetFromCache = new System.Windows.Forms.Button();
+            this.btnAddLogEntry = new System.Windows.Forms.Button();
+            this.grpCache = new System.Windows.Forms.GroupBox();
+            this.btnFlush = new System.Windows.Forms.Button();
+            this.lblCacheValue = new System.Windows.Forms.Label();
+            this.chkCacheHit = new System.Windows.Forms.CheckBox();
+            this.grpLogging = new System.Windows.Forms.GroupBox();
+            this.lblSeverity = new System.Windows.Forms.Label();
+            this.cboSeverity = new System.Windows.Forms.ComboBox();
+            this.lblCacheName = new System.Windows.Forms.Label();
+            this.grpCache.SuspendLayout();
+            this.grpLogging.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGetFromCache
+            // 
+            this.btnGetFromCache.Location = new System.Drawing.Point(17, 53);
+            this.btnGetFromCache.Name = "btnGetFromCache";
+            this.btnGetFromCache.Size = new System.Drawing.Size(109, 23);
+            this.btnGetFromCache.TabIndex = 0;
+            this.btnGetFromCache.Text = "Get from Cache";
+            this.btnGetFromCache.UseVisualStyleBackColor = true;
+            this.btnGetFromCache.Click += new System.EventHandler(this.btnGetFromCache_Click);
+            // 
+            // btnAddLogEntry
+            // 
+            this.btnAddLogEntry.Location = new System.Drawing.Point(67, 66);
+            this.btnAddLogEntry.Name = "btnAddLogEntry";
+            this.btnAddLogEntry.Size = new System.Drawing.Size(109, 23);
+            this.btnAddLogEntry.TabIndex = 1;
+            this.btnAddLogEntry.Text = "Insert Log Entry";
+            this.btnAddLogEntry.UseVisualStyleBackColor = true;
+            this.btnAddLogEntry.Click += new System.EventHandler(this.btnSPLogEntry_Click);
+            // 
+            // grpCache
+            // 
+            this.grpCache.Controls.Add(this.lblCacheName);
+            this.grpCache.Controls.Add(this.btnFlush);
+            this.grpCache.Controls.Add(this.lblCacheValue);
+            this.grpCache.Controls.Add(this.chkCacheHit);
+            this.grpCache.Controls.Add(this.btnGetFromCache);
+            this.grpCache.Location = new System.Drawing.Point(13, 13);
+            this.grpCache.Name = "grpCache";
+            this.grpCache.Size = new System.Drawing.Size(254, 130);
+            this.grpCache.TabIndex = 2;
+            this.grpCache.TabStop = false;
+            this.grpCache.Text = "Cache";
+            // 
+            // btnFlush
+            // 
+            this.btnFlush.Location = new System.Drawing.Point(203, 19);
+            this.btnFlush.Name = "btnFlush";
+            this.btnFlush.Size = new System.Drawing.Size(45, 23);
+            this.btnFlush.TabIndex = 4;
+            this.btnFlush.Text = "Flush";
+            this.btnFlush.UseVisualStyleBackColor = true;
+            this.btnFlush.Click += new System.EventHandler(this.btnFlush_Click);
+            // 
+            // lblCacheValue
+            // 
+            this.lblCacheValue.AutoSize = true;
+            this.lblCacheValue.Location = new System.Drawing.Point(17, 93);
+            this.lblCacheValue.Name = "lblCacheValue";
+            this.lblCacheValue.Size = new System.Drawing.Size(58, 13);
+            this.lblCacheValue.TabIndex = 2;
+            this.lblCacheValue.Text = "-----------------";
+            // 
+            // chkCacheHit
+            // 
+            this.chkCacheHit.AutoSize = true;
+            this.chkCacheHit.Enabled = false;
+            this.chkCacheHit.Location = new System.Drawing.Point(175, 89);
+            this.chkCacheHit.Name = "chkCacheHit";
+            this.chkCacheHit.Size = new System.Drawing.Size(73, 17);
+            this.chkCacheHit.TabIndex = 1;
+            this.chkCacheHit.Text = "Cache Hit";
+            this.chkCacheHit.UseVisualStyleBackColor = true;
+            // 
+            // grpLogging
+            // 
+            this.grpLogging.Controls.Add(this.lblSeverity);
+            this.grpLogging.Controls.Add(this.cboSeverity);
+            this.grpLogging.Controls.Add(this.btnAddLogEntry);
+            this.grpLogging.Location = new System.Drawing.Point(14, 158);
+            this.grpLogging.Name = "grpLogging";
+            this.grpLogging.Size = new System.Drawing.Size(254, 103);
+            this.grpLogging.TabIndex = 3;
+            this.grpLogging.TabStop = false;
+            this.grpLogging.Text = "Logging";
+            // 
+            // lblSeverity
+            // 
+            this.lblSeverity.AutoSize = true;
+            this.lblSeverity.Location = new System.Drawing.Point(13, 32);
+            this.lblSeverity.Name = "lblSeverity";
+            this.lblSeverity.Size = new System.Drawing.Size(48, 13);
+            this.lblSeverity.TabIndex = 3;
+            this.lblSeverity.Text = "Severity:";
+            // 
+            // cboSeverity
+            // 
+            this.cboSeverity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSeverity.FormattingEnabled = true;
+            this.cboSeverity.Location = new System.Drawing.Point(67, 29);
+            this.cboSeverity.Name = "cboSeverity";
+            this.cboSeverity.Size = new System.Drawing.Size(154, 21);
+            this.cboSeverity.TabIndex = 2;
+            // 
+            // lblCacheName
+            // 
+            this.lblCacheName.AutoSize = true;
+            this.lblCacheName.Location = new System.Drawing.Point(17, 25);
+            this.lblCacheName.Name = "lblCacheName";
+            this.lblCacheName.Size = new System.Drawing.Size(58, 13);
+            this.lblCacheName.TabIndex = 5;
+            this.lblCacheName.Text = "-----------------";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 273);
+            this.Controls.Add(this.grpLogging);
+            this.Controls.Add(this.grpCache);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EntLib Extensions";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.grpCache.ResumeLayout(false);
+            this.grpCache.PerformLayout();
+            this.grpLogging.ResumeLayout(false);
+            this.grpLogging.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetFromCache;
+        private System.Windows.Forms.Button btnAddLogEntry;
+        private System.Windows.Forms.GroupBox grpCache;
+        private System.Windows.Forms.CheckBox chkCacheHit;
+        private System.Windows.Forms.GroupBox grpLogging;
+        private System.Windows.Forms.Label lblCacheValue;
+        private System.Windows.Forms.ComboBox cboSeverity;
+        private System.Windows.Forms.Label lblSeverity;
+        private System.Windows.Forms.Button btnFlush;
+        private System.Windows.Forms.Label lblCacheName;
+    }
+}
+
---
+++ EntLibExtensionsTest/Main.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Practices.EnterpriseLibrary.Caching;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System.Diagnostics;
+
+namespace EntLibExtensionsTest
+{
+    /// <summary>
+    /// Main test code
+    /// </summary>
+    public partial class Main : Form
+    {
+        #region "Methods"
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Main()
+        {
+            // Some notes:
+            // To debug a SP2010 console application, change build to AnyCPU
+            // http://rhythmiccoding.blogspot.com/2010/06/retrieving-com-class-factory-for.html
+            // Right click on the ProjectName >> Properties >> Build tab >> Platform target >> Select "Any CPU". (By Default, it's x86)
+            //
+            // Sometimes (if running inside VM) you can get an incorrect cache miss. Create a new cache with NO eviction: New-Cache cacheName -Eviction None
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Show some values upon screen loading
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">EventArgs</param>
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.cboSeverity.DataSource = Enum.GetValues(typeof(TraceEventType));
+            this.cboSeverity.SelectedItem = TraceEventType.Information;
+
+            ICacheManager mgr = CacheFactory.GetCacheManager();
+            string cacheName = (mgr is EntLibExtensions.AppFabricCacheManager)
+                ? ((EntLibExtensions.AppFabricCacheManager)mgr).CacheName
+                : "[Not AppFabric]";
+            this.lblCacheName.Text = String.Format("Cache Name: {0}", cacheName);
+        }
+
+        /// <summary>
+        /// Gets an item from cache
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGetFromCache_Click(object sender, EventArgs e)
+        {
+            this.EnableControls(false);
+            string uniqueKey = "unique_key";
+
+            ICacheManager mgr = CacheFactory.GetCacheManager();
+            object myobject = mgr[uniqueKey];
+            this.chkCacheHit.Checked = (myobject != null);
+            if (myobject == null)
+            {
+                mgr.Add(uniqueKey, "Value retrieved from cache");
+                myobject = "Value retrieved from method";
+            }
+            this.lblCacheValue.Text = (string)myobject;
+            this.EnableControls(true);
+        }
+
+        /// <summary>
+        /// SharePoint 2010 log sample
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">EventArgs</param>
+        private void btnSPLogEntry_Click(object sender, EventArgs e)
+        {
+            this.EnableControls(false);
+            // The following entry is a workaround to use Data Access Block in the GAC
+            // http://entlib.codeplex.com/workitem/26903
+            try
+            {
+                Dictionary<string, object> props = new Dictionary<string, object>();
+                props.Add("property1", DateTime.Now);
+                props.Add("property2", true);
+                props.Add("property3", "String sample");
+
+                Random r = new Random();
+                LogEntry le = new LogEntry();
+                le.ActivityId = Guid.NewGuid();
+                le.Title = "New log entry!";
+                // le.Categories.Add("General");
+                le.Message = String.Format("This is a new log entry to test SPTraceListener... Random:{0}", r.Next(1000).ToString("000"));
+                le.Severity = (TraceEventType)this.cboSeverity.SelectedItem;
+                le.ExtendedProperties = props;
+                Logger.Write(le);
+                // Logger.Write("Title copied from the message");
+                // Logger.Write("Message", "General", 0, 0, TraceEventType.Error, "New title", props);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.ToString());
+            }
+            this.EnableControls(true);
+        }
+
+        /// <summary>
+        /// Attempt to flush the cache from the current Cache Manager
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">EventArgs</param>
+        private void btnFlush_Click(object sender, EventArgs e)
+        {
+            this.EnableControls(false);
+            ICacheManager mgr = CacheFactory.GetCacheManager();
+            // MessageBox.Show(String.Format("Items to be deleted from the cache : {0}", mgr.Count.ToString()));
+            mgr.Flush();
+            this.chkCacheHit.Checked = false;
+            this.lblCacheValue.Text = "";
+            this.EnableControls(true);
+        }
+
+        /// <summary>
+        /// Enable/disable UI controls and set cursor to wait
+        /// </summary>
+        /// <param name="controlsEnabled">Whether to enable/disable controls</param>
+        private void EnableControls(bool controlsEnabled)
+        {
+            this.Enabled = controlsEnabled;
+            this.Cursor = controlsEnabled ? Cursors.Default : Cursors.WaitCursor;
+        }
+
+        #endregion "Methods"
+    }
+}
